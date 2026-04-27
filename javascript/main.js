9ef849b56312c135d81aebe8b61eb145056a041c
raquel-raquel-esperanza <raquel.raquel.esperanza@clases.edu.sv>
2026-04-27 00:59:24 -0600

    Subir nueva carpeta y archivo ejemplo

---
+++ javascript/main.js
@@ -0,0 +1,15 @@
+ // sintaxis de javascript
+/* otro comentario */
+// variables siempre van en minúsculas
+//var y let la diferencia es que var es global y let es local
+var nombre = "Raquel"; //string
+var nombre = "Maria"; //global
+
+let edad = 30; //int
+edad = 15;
+let decimal = 20.2; //double/float
+let esCasado = false; //boolean
+
+//constante siempre van en mayúsculas
+const PI = 3.1416; // double/float
+const SALUDO = "Hola mundo"; //string
