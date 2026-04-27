31c409635a67bdea6afd3a993d87032c4fdea565
raquel-raquel-esperanza <raquel.raquel.esperanza@clases.edu.sv>
2026-04-27 02:44:32 -0600

    ejercicio tabla de multiplicar con for y constante

---
+++ javascript/ejercicio_10.js
@@ -0,0 +1,8 @@
+const numero = 8;
+
+console.log(`--- Tabla del ${numero} ---`);
+
+for (let i = 1; i <= 10; i++) {
+  const resultado = numero * i;
+  console.log(`${numero} x ${i} = ${resultado}`);
+}
